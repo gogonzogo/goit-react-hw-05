66ab82218fee103bc499803fbbfb2803089988e7
Jose Gonzales <jose.gonzales08@ymail.com>
2023-06-01 00:34:01 -0400

    update

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,11 +1,15 @@
 import css from './NotFound.module.css';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
-    <>
-    <h1 className={css.notFoundTitle}>Not Found</h1>
-    </>
-  )
+    <div className={css.notFoundPage}>
+      <h1 className={css.notFoundTitle}>404 Page Not Found</h1>
+      <Link to="/" className={css.homeLink}>
+        Click Here to return to Home
+      </Link>
+    </div>
+  );
 }
 
 export default NotFound;
